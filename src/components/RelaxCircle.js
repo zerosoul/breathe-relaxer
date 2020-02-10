b1b24e8295af973ba263f9f4be0377688e7f5347
Tristan <yanggc888@163.com>
2020-02-10 18:17:16 +0800

    feat: add start button

--- src/components/RelaxCircle.js
+++ src/components/RelaxCircle.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { AniGrow, AniShrink, AniHold, AniRotate } from './Animates';
+import ImagePlay from '../assets/img/play.svg';
+
 const StyledWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -17,6 +19,27 @@ const StyledWrapper = styled.div`
   &.hold {
     animation: ${AniHold} 1.5s linear forwards;
   }
+  &.pause {
+    animation: none;
+  }
+  .startBtn {
+    z-index: 999;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    cursor: pointer;
+    outline: none;
+    background: #fff;
+    border: none;
+    border-radius: 50%;
+    padding: 3.2rem;
+    background-size: 3.4rem 3.4rem;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color: rgba(2, 2, 2, 0.6);
+    background-image: url(${ImagePlay});
+  }
   .tip {
     position: relative;
     width: 2.4rem;
@@ -26,9 +49,12 @@ const StyledWrapper = styled.div`
       position: absolute;
       left: 0;
       top: 0;
-      transition: all 0.5s;
+      transition: all 0.3s;
       display: flex;
       flex-direction: column;
+      &.pause {
+        top: -100%;
+      }
       &.grow {
         transform: translateY(0);
       }
@@ -81,6 +107,7 @@ const StyledWrapper = styled.div`
     transform: translateX(-50%);
     animation: ${AniRotate} 7.5s linear forwards infinite;
     transform-origin: bottom center;
+
     .pointer {
       border-radius: 50%;
       height: 1.6rem;
@@ -103,6 +130,7 @@ const StyledWrapper = styled.div`
 export default function RelaxCircle() {
   const [status, setStatus] = useState('grow');
   const [visible, setVisible] = useState(true);
+  const [start, setStart] = useState(false);
   useEffect(() => {
     document.addEventListener('visibilitychange', function() {
       setVisible(document.visibilityState === 'visible');
@@ -123,23 +151,32 @@ export default function RelaxCircle() {
       setStatus('shrink');
     }
   };
+  const handleStart = () => {
+    console.log('start');
+    document.querySelector('audio').play();
+    setStart(true);
+  };
   return (
     visible && (
-      <StyledWrapper className={status} onAnimationEnd={handleAniEnd}>
+      <StyledWrapper className={`${status} ${start ? '' : 'pause'}`} onAnimationEnd={handleAniEnd}>
         <div className="circle"></div>
-
-        <div className="tip">
-          <p className={`content ${status}`}>
-            <span className="txt">吸气</span>
-            <span className="txt">呼气</span>
-            <span className="txt">屏息</span>
-          </p>
-        </div>
+        {!start && <button className="startBtn" onClick={handleStart}></button>}
+        {start && (
+          <div className="tip">
+            <p className={`content ${status} ${start ? '' : 'pause'}`}>
+              <span className="txt">吸气</span>
+              <span className="txt">呼气</span>
+              <span className="txt">屏息</span>
+            </p>
+          </div>
+        )}
         <div className="gradient-circle"></div>
 
-        <div className="pointer-container">
-          <span className={`pointer ${status}`}></span>
-        </div>
+        {start && (
+          <div className={`pointer-container`}>
+            <span className={`pointer ${status}`}></span>
+          </div>
+        )}
       </StyledWrapper>
     )
   );
