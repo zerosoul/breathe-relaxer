bed1dbc5b06fe81040ad9da01362bdd76ea14302
zerosoul <yanggc888@163.com>
2021-07-08 13:01:40 +0800

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import reactJsx from 'vite-react-jsx'
+import pkg from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: (new URL(pkg.homepage)).pathname,
   server: {
     port: 3002
   },
